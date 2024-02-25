621eed276367667b08f40a7d5b3a15ada553009b
joão vitor <shepherdcom12@gmail.com>
2024-02-25 12:54:53 -0300

    add createpay function

--- pages/api/createpay.js
+++ pages/api/createpay.js
@@ -1,8 +1,31 @@
+const mercadopago = require('mercadopago')
+const client = new mercadopago.MercadoPagoConfig({ accessToken: 'TEST-5990066382783014-022306-c894d2cfa86047cac4dbd4b4efdcb2ef-1694314657' });
+const payment = new mercadopago.Payment(client);
+
 function tempo(req, res) {
-    const dynamicDate = new Date()
-    res.json({
-        date: dynamicDate.toGMTString()
-    })
+    async function createpay() {
+        try {
+            const result = await payment.create({
+                body: {
+                    transaction_amount: 12.34,
+                    description: '<DESCRIPTION>',
+                    payment_method_id: 'pix',
+                    payer: {
+                        email: '<EMAIL>'
+                    },
+                }
+            });
+            return result;
+        } catch (error) {
+            return error;
+        }
+    }
+    createpay()
+        .then(result => {
+            res.json({
+                result
+            })
+        })
 }
 
 export default tempo;
