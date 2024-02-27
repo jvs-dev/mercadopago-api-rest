af1970b9bebd28afe7348a22e6393e177023cf8b
joão vitor <shepherdcom12@gmail.com>
2024-02-26 22:22:05 -0300

    add no cors

--- pages/api/createpay.js
+++ pages/api/createpay.js
@@ -1,31 +1,40 @@
-const mercadopago = require('mercadopago')
-const client = new mercadopago.MercadoPagoConfig({ accessToken: 'TEST-5990066382783014-022306-c894d2cfa86047cac4dbd4b4efdcb2ef-1694314657' });
-const payment = new mercadopago.Payment(client);
+const mercadopago = require('mercadopago');
+const { MercadoPagoConfig, Payment } = mercadopago;
 
-function tempo(req, res) {
-    async function createpay() {
-        try {
-            const result = await payment.create({
-                body: {
-                    transaction_amount: 12.34,
-                    description: '<DESCRIPTION>',
-                    payment_method_id: 'pix',
-                    payer: {
-                        email: '<EMAIL>'
-                    },
-                }
-            });
-            return result;
-        } catch (error) {
-            return error;
-        }
+const client = new MercadoPagoConfig({ accessToken: 'TEST-5990066382783014-022306-c894d2cfa86047cac4dbd4b4efdcb2ef-1694314657' });
+const payment = new Payment(client);
+
+async function createpay() {
+    try {
+        const result = await payment.create({
+            body: {
+                transaction_amount: 12.34,
+                description: '<DESCRIPTION>',
+                payment_method_id: 'pix',
+                payer: {
+                    email: '<EMAIL>'
+                },
+            }
+        });
+        return result;
+    } catch (error) {
+        return error;
     }
-    createpay()
-        .then(result => {
-            res.json({
-                result
-            })
-        })
 }
 
-export default tempo;
+const init = async (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*'); // Permitir solicitações de qualquer origem
+
+    if (req.method === 'OPTIONS') {
+        // Permitir métodos e cabeçalhos personalizados
+        res.setHeader('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
+        res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+        res.status(200).end();
+        return;
+    }
+
+    const result = await createpay();
+    res.json({ result });
+};
+
+export default init;
